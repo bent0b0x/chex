caca4887e6fa616779e7f5331a74de85ad300e3a
Benjamin Baum <bjb253@cornell.edu>
2016-06-28 22:20:51 -0400

    Refactor board reducer

--- client/src/js/app/game/board/boardReducer.js
+++ client/src/js/app/game/board/boardReducer.js
@@ -2,18 +2,22 @@ import initialState from '../../../app/initialState';
 import * as SpaceActions from './space/SpaceActions';
 import spaceReducer from './space/spaceReducer';
 
+const handleSelect = (space, action) => {
+  if (!space.piece || !space.selected && (space.row !== action.row || space.col !== action.col)) {
+    return space;
+  }
+  return spaceReducer(space, action);
+};
+
 export default (state = initialState.game.board, action) => {
   return state.map(row => (
     row.map(space => {
       switch (action.type) {
         case SpaceActions.SELECT:
-          if (!space.piece || !space.selected && (space.row !== action.row || space.col !== action.col)) {
-            return space;
-          }
-          return spaceReducer(space, action);
+          return handleSelect(space, action);
         default:
           return space;
       }
     })
-  ))
+  ));
 };
