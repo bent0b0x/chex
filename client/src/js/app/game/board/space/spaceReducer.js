e70de6e54f588c4e0c7e54645cd2b3fda3adf90c
Benjamin Baum <bjb253@cornell.edu>
2016-06-28 21:17:40 -0400

    Remove logging statement

--- client/src/js/app/game/board/space/spaceReducer.js
+++ client/src/js/app/game/board/space/spaceReducer.js
@@ -1,7 +1,6 @@
 import * as Actions from './SpaceActions';
 
 export default (state, action) => {
-  console.log('reducing space');
   switch (action.type) {
     case Actions.SELECT:
       return Object.assign({}, state, {selected: !state.selected});
